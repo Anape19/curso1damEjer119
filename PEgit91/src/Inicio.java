295a36b1e635b1e8d938a55089252a4afd1339cb
ana <teaproa@gmail.com>
2019-04-11 07:36:01 +0200

    Cambio a Egit 2

--- PEgit91/src/Inicio.java
+++ PEgit91/src/Inicio.java
@@ -3,7 +3,7 @@ public class Inicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Mundo Egit 1");
+		System.out.println("Mundo Egit 2");
 
 	}
 
